ff9ffa2b8b44eb1ac272c5edf7e0f7516bfc7409
Javi-zafiro <javialexiscortez2@gmail.com>
2023-06-20 22:37:24 -0300

    ejercicio 2

--- Program.cs
+++ Program.cs
@@ -1,84 +1,26 @@
-﻿using Tareas;
-string? buscar;
-Tarea encontrada=null;
-List<Tarea> pendientes= new List<Tarea>();
-List<Tarea> realizadas= new List<Tarea>();
-int n, x, y, i;
-char respuesta;
-var random= new Random(Environment.TickCount);
+﻿Console.WriteLine("Ingrese la ruta de la carpeta:");
+string carpeta = Console.ReadLine();
 
-Console.WriteLine("ingrese la cantidad de tarea");
-int.TryParse(Console.ReadLine(),out n);
-
-for ( i = 0; i < n; i++)
+string[] archivos = Directory.GetFiles(carpeta); //matriz de los archivos que estan en el path
+Console.WriteLine("Listado de archivos:");
+foreach (string archivo in archivos) //recorre el arreglo y lo lista
 {
-    x=random.Next(0, 3);
-    y=random.Next(10, 100);
-    pendientes.Add(new Tarea(i+1,x,y));
+    Console.WriteLine(archivo);
 }
-Console.WriteLine("listemos las tareas y veamos si se realizaron o no");
-i = 0;
-while (i < pendientes.Count)
+
+string archivoCSV = Path.Combine(carpeta, "index.csv");
+
+using (StreamWriter escritor = new StreamWriter(archivoCSV))
 {
-    Console.WriteLine(pendientes[i].TareaID);
-    Console.WriteLine(pendientes[i].Descripcion);
-    Console.WriteLine(pendientes[i].Duracion);
-    var item = pendientes[i];
-    Console.WriteLine("¿Esta tarea se realizó? (Y/N)");
-    char.TryParse(Console.ReadLine(), out respuesta);
-    if (respuesta == 'Y' || respuesta == 'y')
+    escritor.WriteLine("Índice,Nombre,Extensión");
+
+    for (int i = 0; i < archivos.Length; i++)
     {
-        pendientes.Remove(item);
-        realizadas.Add(item);
-    }
-    else
-    {
-        i++; 
+        string nombreArchivo = Path.GetFileName(archivos[i]);
+        string extensionArchivo = Path.GetExtension(archivos[i]);
+
+        escritor.WriteLine($"{i + 1},{nombreArchivo},{extensionArchivo}");
     }
 }
 
-Console.WriteLine("lista de pendientes");
-foreach (var item in pendientes)
-{
-    Console.WriteLine(item.TareaID);
-    Console.WriteLine(item.Descripcion);
-    Console.WriteLine(item.Duracion);
-}
-
-Console.WriteLine("lista de realizadas");
-foreach (var item in realizadas)
-{
-    Console.WriteLine(item.TareaID);
-    Console.WriteLine(item.Descripcion);
-    Console.WriteLine(item.Duracion);
-}
-
- Console.WriteLine("Ingrese una descripción para buscar tareas pendientes:");
-buscar = Console.ReadLine();
-foreach (var item in pendientes)
-{
-    string descripcion=item.Descripcion;
-    if (buscar == descripcion)
-    {
-        encontrada= item;
-    }
-}
-if (encontrada!=null)
-{
-    Console.WriteLine("se encontro la tarea");
-    Console.WriteLine(encontrada.TareaID);
-    Console.WriteLine(encontrada.Descripcion);
-    Console.WriteLine(encontrada.Duracion);
-}else
-{
-    Console.WriteLine("no se encontro la tarea");
-}
-int sumatoria=0;
-foreach (var item in realizadas)
-{
-    sumatoria=+item.Duracion;
-}
-using (StreamWriter sw= new StreamWriter("sumatoria.txt"))
-{
-    sw.WriteLine(sumatoria);
-}
+Console.WriteLine("Listado de archivos guardado en index.csv");
