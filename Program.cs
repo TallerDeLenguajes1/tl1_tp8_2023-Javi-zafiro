b06b0a404232f3cd0fb8f336fdc2369348168886
Javi-zafiro <javialexiscortez2@gmail.com>
2023-06-20 22:12:13 -0300

    punto 1 completo

--- Program.cs
+++ Program.cs
@@ -1,32 +1,39 @@
 ﻿using Tareas;
+string? buscar;
+Tarea encontrada=null;
 List<Tarea> pendientes= new List<Tarea>();
 List<Tarea> realizadas= new List<Tarea>();
-int n, x, y;
+int n, x, y, i;
 char respuesta;
 var random= new Random(Environment.TickCount);
 
 Console.WriteLine("ingrese la cantidad de tarea");
 int.TryParse(Console.ReadLine(),out n);
 
-for (int i = 0; i < n; i++)
+for ( i = 0; i < n; i++)
 {
     x=random.Next(0, 3);
     y=random.Next(10, 100);
     pendientes.Add(new Tarea(i+1,x,y));
 }
 Console.WriteLine("listemos las tareas y veamos si se realizaron o no");
-for (int i = 0; i < n; i++)
+i = 0;
+while (i < pendientes.Count)
 {
     Console.WriteLine(pendientes[i].TareaID);
     Console.WriteLine(pendientes[i].Descripcion);
     Console.WriteLine(pendientes[i].Duracion);
-    var item=pendientes[i];
-    Console.WriteLine("esta tarea se realizo? (Y/N)");
+    var item = pendientes[i];
+    Console.WriteLine("¿Esta tarea se realizó? (Y/N)");
     char.TryParse(Console.ReadLine(), out respuesta);
-    if (respuesta=='Y' || respuesta=='y')
+    if (respuesta == 'Y' || respuesta == 'y')
     {
         pendientes.Remove(item);
         realizadas.Add(item);
+    }
+    else
+    {
+        i++; 
     }
 }
 
@@ -44,4 +51,34 @@ foreach (var item in realizadas)
     Console.WriteLine(item.TareaID);
     Console.WriteLine(item.Descripcion);
     Console.WriteLine(item.Duracion);
+}
+
+ Console.WriteLine("Ingrese una descripción para buscar tareas pendientes:");
+buscar = Console.ReadLine();
+foreach (var item in pendientes)
+{
+    string descripcion=item.Descripcion;
+    if (buscar == descripcion)
+    {
+        encontrada= item;
+    }
+}
+if (encontrada!=null)
+{
+    Console.WriteLine("se encontro la tarea");
+    Console.WriteLine(encontrada.TareaID);
+    Console.WriteLine(encontrada.Descripcion);
+    Console.WriteLine(encontrada.Duracion);
+}else
+{
+    Console.WriteLine("no se encontro la tarea");
+}
+int sumatoria=0;
+foreach (var item in realizadas)
+{
+    sumatoria=+item.Duracion;
+}
+using (StreamWriter sw= new StreamWriter("sumatoria.txt"))
+{
+    sw.WriteLine(sumatoria);
 }
